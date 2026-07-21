2dbbad7e43dd9745e8da35bc8d7b6fec6e5bea7f
Arash Nassirpour <arash.nassirpour@gmail.com>
2026-07-21 10:28:45 +0100

    Map all Food for your Soul images on views page

--- direct-image-test.js
+++ direct-image-test.js
@@ -1,10 +1,11 @@
 (() => {
   const path = window.location.pathname;
   const isExplore = path === "/explore/" || path === "/explore/index.html";
+  const isViews = path === "/views/" || path === "/views/index.html";
   const isFood = path === "/breakfast/" || path === "/breakfast/index.html" || path === "/restaurants/" || path === "/restaurants/index.html";
   const isShopping = path === "/shopping/" || path === "/shopping/index.html";
   const isApps = path === "/apps/" || path === "/apps/index.html";
-  if (!isExplore && !isFood && !isShopping && !isApps) return;
+  if (!isExplore && !isViews && !isFood && !isShopping && !isApps) return;
 
   function applyPhoto(title, photo, alternatives = []) {
     const titles = [title, ...alternatives];
@@ -50,7 +51,7 @@
     });
   }
 
-  function applyExplore() {
+  function applySoulPhotos() {
     applyPhoto("F1 Exhibition", "/images/8ff9674219704d66c64661d38e528f3f.jpg");
     applyPhoto("Lift 109", "/images/0d7e295df686fe1e8fe1ddf003a41794.jpg");
     applyPhoto("V&A Museum", "/images/182e732c1dbeb2ffc60b3a48e3bc7729.jpg", ["V&A"]);
@@ -60,6 +61,7 @@
     applyPhoto("Tate Modern", "/images/4928f976e7b52409afec4ab61092cee5.jpg");
     applyPhoto("Design Museum", "/images/590c6f0945d0d93cef5c726c9419abfa.jpg", ["The Design Museum"]);
     applyPhoto("Leighton House", "/images/5b427e328ceb6e89d8752d8e7acfc4f1.jpg", ["Leighton House Museum"]);
+    applyPhoto("British Museum", "/images/66f907155dfc2a28b81a8a22527eabe4.jpg", ["The British Museum"]);
     applyPhoto("BFI IMAX", "/images/b1a57b35cafb5213490873c5e0a88418.jpg");
     applyPhoto("Sir John Soane’s Museum", "/images/bcdcf2460868566f19ee29f1435d9823.jpg", ["Sir John Soane's Museum"]);
     applyPhoto("Horizon 22", "/images/bf1a743a1ec2aae319c073e4b7bd7753.jpg");
@@ -67,6 +69,14 @@
     applyPhoto("The Wallace Collection", "/images/ec7c4ad259ef017c4671665e36aff59a.jpg", ["Wallace Collection"]);
     applyPhoto("The Garden at 120", "/images/f565cffccee2cd21103ac5db6e06a88b.jpg", ["Garden at 120"]);
     applyPhoto("Royal Academy of Arts", "/images/f79512cfb1ceef72be71da036b92318d.jpg", ["Royal Academy"]);
+  }
+
+  function applyExplore() {
+    applySoulPhotos();
+  }
+
+  function applyViews() {
+    applySoulPhotos();
   }
 
   function applyFood() {
@@ -118,6 +128,7 @@
 
   function applyAll() {
     if (isExplore) applyExplore();
+    if (isViews) applyViews();
     if (isFood) applyFood();
     if (isShopping) applyShopping();
     if (isApps) applyApps();
